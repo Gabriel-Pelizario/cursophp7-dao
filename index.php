94e02f096ca87dae6e46e7ee2617f157953d7f38
Gabriel Pelizario <gabrielpelizario2013@gmail.com>
2021-11-04 14:43:04 -0300

    inserir e alterar

--- index.php
+++ index.php
@@ -2,10 +2,37 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//carrega um usuario
+//$root = new Usuario();
+//$root->loadById(8);
+//echo $root;
 
-$root->loadById(8);
+//carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//carrega um usuário usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("Tobias","444999");
+//echo $usuario;
+
+//insert com procedure
+//$aluno = new Usuario("Aluno", "Root");
+//$aluno->insert();
+//echo $aluno;
+
+$usuario = new Usuario();
+
+$usuario->loadById(8);
+
+$usuario->update("professor", "!@#%&*");
+
+echo $usuario;
+
+
 
  ?>
